e4c99b9c9bb9a7b5315e1bc4e040bc62d67f8a37
Tarif Ezaz <tarif.ezaz@gmail.com>
2021-05-17 17:17:58 +0600

    Add stack navigation

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import 'react-native-gesture-handler';
 import { 
   StyleSheet, 
-  Text, 
-  View 
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from "./src/screens/Home/Home"
+import Splash from "./src/screens/Splash"
+import Details from "./src/screens/Details/Details"
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen options={{headerShown: false}} name="Splash" component={Splash} />
+          <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
+          <Stack.Screen options={{headerShown: false}} name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
