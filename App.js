f15e8a6beaf1a40d49ba50ca124c53af874038fb
Tarif Ezaz <tarif.ezaz@gmail.com>
2021-05-17 19:13:34 +0600

    Remove redundant codes and refactor

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import 'react-native-gesture-handler';
-import { 
-  StyleSheet, 
-} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -17,19 +13,22 @@ export default function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen options={{headerShown: false}} name="Splash" component={Splash} />
-          <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
-          <Stack.Screen options={{headerShown: false}} name="Details" component={Details} />
+          <Stack.Screen 
+            options={{headerShown: false}} 
+            name="Splash" 
+            component={Splash} 
+          />
+          <Stack.Screen 
+            options={{headerShown: false}} 
+            name="Home" 
+            component={Home} 
+          />
+          <Stack.Screen 
+            options={{headerShown: false}} 
+            name="Details" 
+            component={Details} 
+          />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
