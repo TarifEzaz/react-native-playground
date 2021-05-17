7bf91ae40573bcdcd5abead934d4269161710ecf
Tarif Ezaz <tarif.ezaz@gmail.com>
2021-05-17 23:58:30 +0600

    Refactor component

--- src/screens/Home/DetailsScreen.js
+++ src/screens/Home/DetailsScreen.js
@@ -15,6 +15,7 @@ import AmenitiesList from "./Components/AmenitiesList"
 export default function App({ navigation, route}) {
   const { item } = route.params
   const [readMore, setReadMore] = useState(false)
+
   return (
     <ImageBackground 
       source={{uri: `${config.baseUrl}/${item.photo}`}}
@@ -62,18 +63,7 @@ export default function App({ navigation, route}) {
           <AmenitiesList amenities={item.amenities}/>
         </View>   
       </ScrollView>
-      <View style={styles.bookingContainer}>
-        <View style={styles.bookingView}>
-          <Text style={styles.priceTxt}>
-            {`$${item.price}/night`}
-          </Text>
-          <TouchableOpacity style={styles.bookingBtn}>
-            <Text style={styles.bookingBtnTxt}>
-              Request To Book
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <BookingArea />
     </ImageBackground>
   );
 }
@@ -158,42 +148,4 @@ const styles = StyleSheet.create({
   readMoreTxt: {
     color: config.colors.accent,
   },
-  bookingContainer:{
-    backgroundColor:"white",
-    width:"100%",
-    height:100,
-  },
-  bookingView: {
-    justifyContent:"center",
-    alignItems:"center",
-    flexDirection:"row",
-    height:100,
-    backgroundColor:"white",
-    borderTopRightRadius: 50,
-    borderTopLeftRadius: 50,
-    shadowColor: "#4E4466",
-    shadowOffset: {
-      width: 0,
-      height: -5,
-    },
-    shadowOpacity: .1,
-    shadowRadius: 20,
-    elevation: 8,
-  },
-  priceTxt: {
-    color:"#8F87A5",
-    fontFamily: "Rubik_400Regular",
-    fontSize:17,
-  },
-  bookingBtn: {
-    backgroundColor:config.colors.accent,
-    borderRadius: 30,
-    padding: 15,
-    marginLeft: 10
-  },
-  bookingBtnTxt:{
-    color: "white",
-    fontFamily:"Rubik_500Medium",    
-    fontSize: 14,
-  }
 })
