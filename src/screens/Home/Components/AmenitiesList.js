2d101203e1e0bfe20cd0da8b889eb27a93a58432
Tarif Ezaz <tarif.ezaz@gmail.com>
2021-05-18 19:42:16 +0600

    Remove redundant import

--- src/screens/Home/Components/AmenitiesList.js
+++ src/screens/Home/Components/AmenitiesList.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { 
     Image,
     StyleSheet,
     Text,
     TouchableOpacity
 } from "react-native";
+
 import config from "../../../config";
 
 const AmenitiesList = ({ item, onPress }) => {
