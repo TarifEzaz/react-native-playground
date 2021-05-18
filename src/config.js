f8d65e0468cb6d5598298ab77f6e7c2bf6e8cf0f
Tarif Ezaz <tarif.ezaz@gmail.com>
2021-05-18 16:07:31 +0600

    Define config variable

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
-export default config = {
-    colors: {
-      accent: "#F72585",
-      primary: "#3A0CA3",
-      secondary: "#2F0F79"
-    },
-    baseUrl: "http://192.168.1.21:3000"
-  }
+const config = {
+  colors: {
+    accent: "#F72585",
+    primary: "#3A0CA3",
+    secondary: "#2F0F79"
+  },
+  baseUrl: "http://192.168.1.5:3000",
+}
+
+export default config
